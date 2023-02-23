8e415b38edfc740965913734a0ef23bfb916554a
Julian Lueken <julian.lueken@stud.uni-goettingen.de>
2023-02-23 21:43:35 +0100

    yeah

--- nlptest/read_dataset.py
+++ nlptest/read_dataset.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import spacy
 from spacy import displacy
+import wikipediaapi
+
+wiki = wikipediaapi.Wikipedia("en")
+page = wiki.page("Warner Bros")
 
 nlp = spacy.load("en_core_web_sm")
-
-introduction_doc = nlp("Keanu Reeves is an Actor who played in the movie 'The Matrix' in 1999.")
-print([token.text for token in introduction_doc])
-
-displacy.serve(introduction_doc, style="ent")
+doc = nlp(page.text)
+displacy.serve(doc, style="ent", port=1000)
